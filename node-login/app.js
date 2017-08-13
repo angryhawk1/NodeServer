2c443e6fae3565ea0ba13d62fb5006da64a73ee4
Abdul-Razzak <abd.razzak.shaikh@gmail.com>
2017-08-13 23:40:05 +0000

    add more

--- node-login/app.js
+++ node-login/app.js
@@ -6,11 +6,21 @@ const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
 const port 	   = process.env.PORT || 3000;
+var config = {
+    'secrets' : {
+        'clientId' : '42GW252KX3YS4AGGFQN2IKF0JDQMYSDBBPOQYDQRZGU3KLZH',
+        'clientSecret' : '5DPKWXGF4WA31FUPY1GP0DMYKQQWZ50P1IA2O4R5XR2EF4HS',
+        'redirectUrl' : 'http://ec2-18-220-154-55.us-east-2.compute.amazonaws.com:3000/callback' // This should also be set in your OAuth profile.
+    }
+};
+var foursquare = require('./lib/node-foursquare')(config);
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-require('./routes')(router);
+require('./route/userRoutes')(router);
+require('./route/places')(router, foursquare, config);
+
 app.use('/', router);
 
 app.listen(port);
